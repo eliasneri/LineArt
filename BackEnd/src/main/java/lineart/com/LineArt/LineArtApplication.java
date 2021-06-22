da08ebeb6bce16bf632baedeffc3962f0a8f7c81
eliasneri <eliasneri@hotmail.com>
2021-06-22 10:04:47 -0300

    Project Created

---
+++ BackEnd/src/main/java/lineart/com/LineArt/LineArtApplication.java
@@ -0,0 +1,13 @@
+package lineart.com.LineArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineArtApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/lineart/com/LineArt/LineArtApplicationTests.java
@@ -0,0 +1,13 @@
+package lineart.com.LineArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
